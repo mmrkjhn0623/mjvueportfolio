b68138e82a6afbe6d1ab1412a66b4723ec1a83c6
mmrkjhn <mmrkjhn@gmail.com>
2025-07-29 11:28:38 +0800

    update homepage for portfolio

--- src/router.ts
+++ src/router.ts
@@ -1,29 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
-import Tutorial from './views/Tutorial.vue'
-import Tutorial1 from './views/Tutorial1.vue'
-import Tutorial2 from './views/Tutorial2.vue'
-import LoginFirst from './views/LoginFirst.vue'
-import LoginForm from './views/LoginForm.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/tutorial', name: 'Tutorial', component: Tutorial,
-    children: [
-      {
-        path: '1',
-        component: Tutorial1,
-        meta: { requiresAuth: true }
-      },
-      {
-        path: '2',
-        component: Tutorial2,
-        meta: { requiresAuth: true }
-      }
-    ] 
-  },
-  { path: '/loginfirst', name: 'LoginFirst', component: LoginFirst},
-  { path: '/login', name: 'Login', component: LoginForm}
+  { path: '/', name: 'Home', component: Home }
 ]
 
 const router = createRouter({
